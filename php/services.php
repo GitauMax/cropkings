c2cc7a2b15e86d0acfa70cb1db7f4bbe26615062
Maxwell Gitau Mmbasu <104240993+GitauMax@users.noreply.github.com>
2022-09-10 20:40:07 +0300

    Add files via upload

---
+++ php/services.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>services</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">Grow&Glow</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="services.php">services</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/web7.jpg) no-repeat">
+   <h1>services</h1>
+</div>
+
+<!-- packages section starts  -->
+
+<section class="packages">
+
+   <h1 class="heading-title">our services</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/web6.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>AGRONOMY TRAININGS</h3>
+            <p>Farmers need trainings to make informed growing decisions. 
+               At Grow and Glow Agribusiness, we have a team of Agronomists experts that  will walk the crop with you and advice you on the best approaches for optimum yield and cost-effective production.
+             They will share knowledge of agronomy techniques and skills that can improve your productivity
+            </p>
+         
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/web12.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>SCIENTIFIC TRIALS</h3>
+            <p>We conduct field trials for seed varieties and agro-chemicals to determine 
+               the most suitable crop varieties and effective agro-chemicals for crop production.
+                Monitoring and evaluation of the trials is keenly done, data collected and analyzed using GenStat or SAS and resulted presented.
+                Additionally, we conduct field demonstrations. Our trial team is independent and as such only recommends products based on performance.</p>
+           
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/ser1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>CROP PATHOLOGY</h3>
+            <p>The most critical issue for profitable management of crop diseases is getting the correct diagnosis. 
+               We provide disease diagnostic services. Disease development in plants is influenced 
+               by many factors including, variety genetics of the crop, age of the plant, environment 
+               (Weather, Soil, Farm Operations etc.), single versus mixed infections, and the pathogen populations genetics.
+</p>
+           
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/ser2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>CROP NUTRITION</h3>
+            <p>We promote using the right fertilizer source, at the right rate, at the right time,
+                and in the right place so as to increase yields and profitability
+                while protecting the environment and improving sustainability.
+                 Crops thrive from receiving proper nutrition more efficiently.</p>
+           
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/ser3.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>CROP ENTERPRISE MANAGEMENT</h3>
+            <p>At grow and glow agribusiness, we provide best solution to agribusiness entrepreneur. 
+               We offer professional advice on venture choice, 
+               cost-benefit analysis to business plan and overall farm management.
+                We provide advises in good farm management.</p>
+           
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/web11.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>BUSINESS DEVELOPMENT</h3>
+            <p>We assist agro-chemical companies in identification and capturing 
+               new business and market opportunities for either products, services or new R&D outcome. 
+               We will link your company to new growth markets by exposing them to smallholder farmers.</p>
+            
+         </div>
+      </div>
+
+    <!--  <div class="box">
+         <div class="image">
+            <img src="images/img-7.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-8.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-9.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-10.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-11.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-12.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>adventure & tour</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, perspiciatis!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"><span class="btn">load more</span></div> -->
+
+</section>
+
+<!-- packages section ends -->
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+       
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i>254705534199</a>
+         <a href="#"> <i class="fas fa-envelope"></i> consultgrow-glow@proton.me </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+  
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
